2627b153b1ad2855d639677d487dd7f415a150be
Boudewijn van Groos <boudewijn@vangroos.nl>
2023-03-20 11:39:37 +0000

    Set up vuetify HMR

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,37 @@
-import vuetify, { transformAssetUrls } from "vite-plugin-vuetify";
+const hmrPort = 24678;
+const hmrHost = `${process.env.CODESPACE_NAME}-${hmrPort}.${process.env.GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN}`;
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  css: ["vuetify/lib/styles/main.sass"],
+  build: {
+    transpile: ["vuetify"],
+  },
   vite: {
-    plugins: [vuetify({ autoImport: true })],
+    define: {
+      "process.env.DEBUG": false,
+    },
+    server: {
+      hmr: {
+        protocol: "wss",
+        clientPort: 443,
+        port: hmrPort,
+      },
+    },
+    plugins: [
+      {
+        name: "client-host",
+        transform(code, id) {
+          if (
+            id.endsWith("dist/client/client.mjs") ||
+            id.endsWith("dist/client/env.mjs")
+          ) {
+            return code.replace("__HMR_HOSTNAME__", JSON.stringify(hmrHost));
+          }
+
+          return code;
+        },
+      },
+    ],
   },
 });
